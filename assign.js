f5922a86c0d7280935d91fa19a818cb854366143
FinnSpartronics <junkjunk.pump@gmail.com>
2025-01-23 21:40:00 -0800

    wip

--- assign.js
+++ assign.js
@@ -1,5 +1,5 @@
-document.querySelector("textarea#members").value = "a\nb\nc\nd\ne\nf\ng\nh\ni(1-20)"
-document.querySelector("textarea#positions").value = "1\n2\n3\n4\n5\n6"
+document.querySelector("textarea#members").value = "a\nb\nc\nd\ne\nf\ng\nh[40-60-1,1-20-1](1-20)\ni(1-20)"
+document.querySelector("textarea#positions").value = "Red Front\nRed Middle\nRed Back\nBlue Front\nBlue Middle\nBlue Back"
 document.querySelector("#match_count").value = "60"
 document.querySelector("#session_length").value = "10"
 
@@ -16,12 +16,11 @@ function go() {
 
     let positions = document.querySelector("textarea#positions").value.split("\n")
     for (let x of positions) x = x.trim()
-    console.log(positions)
 
     let restrictions = {}
     for (let i in teamMembers) {
         if (teamMembers[i].includes("(")) {
-            let unfiltered = teamMembers[i].substring(teamMembers[i].indexOf("("), teamMembers[i].length).trim().replace("(", "").replace(")", "")
+            let unfiltered = teamMembers[i].substring(teamMembers[i].indexOf("("), teamMembers[i].indexOf(")")).trim().replace("(", "").replace(")", "")
             let split = unfiltered.split(",")
             for (let filter in split) {
                 split[filter] = split[filter].split("-")
@@ -32,110 +31,75 @@ function go() {
         }
     }
 
-    let matchesNeeded = parseInt(document.querySelector("#match_count").value) * positions.length
-
-    let preferredSessionLength = parseInt(document.querySelector("#session_length").value) - 1
-
-    output.innerText += teamMembers.length + " members, " + matchesNeeded + " matches" + ", ~" + matchesNeeded/preferredSessionLength + " sessions\n\n"
-
-    let sessions = []
-    let memberOn = 0
-    while (sessions.length < matchesNeeded/preferredSessionLength) {
-        sessions.push(teamMembers[memberOn])
-        memberOn = (memberOn + 1) % teamMembers.length
-    }
-    output.innerText += sessions + "\n\n"
-
-    let sessionMatches = []
-    for (let i = 0; i < positions.length; i++) sessionMatches.push([0, 2 * (i+1), positions[i]])
-
-    while (sessionMatches[sessionMatches.length-positions.length][1] < matchesNeeded / positions.length) {
-        let previous = sessionMatches[sessionMatches.length - positions.length][1] + 1
-
-        let ending = Math.min(previous + preferredSessionLength, matchesNeeded / positions.length - 1)
-        if (ending >= matchesNeeded / positions.length - 3) ending = matchesNeeded / positions.length
-        sessionMatches.push([previous, ending, sessionMatches[sessionMatches.length-positions.length][2]])
-    }
-
-    for (let match of sessionMatches) {
-        match[0]++
-        match[1]++
-        match[1] = Math.min(match[1], matchesNeeded / positions.length)
-        output.innerText += "[" + match[0] + "," + match[1] + ", " + match[2] + "], "
-    }
-
-    if (showDebug)
-        output.innerText += "\n\n----- Final Output -----\n\n"
-    else
-        output.innerText = " "
-
-    console.log(restrictions)
-    // Returns false if valid, true if not
-    function checkRestrictions(start, end, member) {
-        if (Object.keys(restrictions).includes("" + member)) {
-            let list = restrictions["" + member]
-            for (let filter of list) {
-                if (start >= filter[0] && end <= filter[1]) return false;
+    let forcedMatches = {}
+    for (let i in teamMembers) {
+        if (teamMembers[i].includes("[")) {
+            let unfiltered = teamMembers[i].substring(teamMembers[i].indexOf("["), teamMembers[i].indexOf("]")).trim().replace("[", "").replace("]", "")
+            let split = unfiltered.split(",")
+            for (let filter in split) {
+                split[filter] = split[filter].split("-")
+                split[filter][0] = parseInt(split[filter][0].trim())
+                split[filter][1] = parseInt(split[filter][1].trim())
             }
-            return true;
-        } else return false;
+            forcedMatches[i] = split
+        }
     }
+    console.log("forced", forcedMatches)
+
+    let matchesNeeded = parseInt(document.querySelector("#match_count").value)
+    let preferredSessionLength = parseInt(document.querySelector("#session_length").value)
+
+    output.innerText += teamMembers.length + " members, " + matchesNeeded + " matches\n\n"
 
     let teamMember = 0;
     let teamMemberOutputs = {}
     let teamMemberSessions = {}
-    for (let x = 0; x < teamMembers.length; x++) {
-        teamMemberOutputs[x] = teamMembers[x] + ": "
-        teamMemberSessions[x] = []
-    }
-    for (let m = 0; m < sessionMatches.length; m++) {
-        while (checkRestrictions(sessionMatches[m][0], sessionMatches[m][1], teamMember))
-            teamMember = (teamMember + 1) % teamMembers.length
-        teamMemberOutputs[teamMember] += " [" + sessionMatches[m][0] + "," + sessionMatches[m][1] + "," + sessionMatches[m][2] + "],"
-        teamMemberSessions[teamMember].push(sessionMatches[m])
-        teamMember = (teamMember + 1) % teamMembers.length
-    }
-    console.log(teamMemberOutputs)
 
-    let missedMatchesTest = {}
-    for (let i = 1; i <= matchesNeeded/positions.length; i++) {
-        missedMatchesTest[i] = 0
+    for (teamMember in teamMembers) {
+        teamMemberSessions[teamMember] = []
+        if (forcedMatches[teamMember] !== undefined) {
+            for (let forced of Object.values(forcedMatches[teamMember])) teamMemberSessions[teamMember].push(forced)
+        }
     }
 
-    for (let x in teamMemberSessions) {
-        let name = teamMembers[x]
-        if (name.includes("(")) name = name.substring(0, name.indexOf("(")).trim()
-        output.innerText += name + ": "
-        
-        let matchesScouted = 0
-        let lastMatch = -100;
-        let overlap = false;
+    let matchAssignments = []
+    for (let position = 0; position < positions.length; position++) {
+        let arr = []
 
-        for (let session of teamMemberSessions[x]) {
-            output.innerText += " " + JSON.stringify(session) + ","
-            matchesScouted += session[1] - session[0] + 1
-            for (let i = session[0]; i <= session[1]; i++) {
-                missedMatchesTest[i]++;
+        let takenSlots = []
+        for (teamMember in teamMembers) {
+            if (forcedMatches[teamMember] !== undefined) {
+                for (let forced of Object.values(forcedMatches[teamMember])) {
+                    if (forced[2] == position) takenSlots.push(forced)
+                }
+            }
+        }
+        takenSlots.sort((a, b) => {return a[0] - b[0]})
+        console.log(positions[position], takenSlots)
+
+        arr.push([1, 2 + Math.ceil(1.4 * Math.random() * (preferredSessionLength - 2))])
+        for (let match = arr[arr.length - 1][1] + 1 + preferredSessionLength; match < matchesNeeded; match += preferredSessionLength) {
+            if (takenSlots.length > 0) {
+                let curr = -1
+                let i = 0
+                while (takenSlots[i][0] <= match) {
+                    if (takenSlots[i][0] <= match) curr = takenSlots[i]
+                    i++
+                }
+                if (match < curr[1]) {
+                    arr.push(curr)
+                    match = curr[1]
+                    continue
+                }
             }
 
-            if (lastMatch > session[1]) overlap = true;
-            lastMatch = session[1]
+            arr.push([arr[arr.length - 1][1] + 1, match])
         }
+        arr.push([arr[arr.length - 1][1] + 1, matchesNeeded])
 
-        output.innerText += " (" + matchesScouted + " matches)"
-        if (overlap)
-            output.innerText += "     (OVERLAP)"
-        output.innerText += "\n"
-        
+        matchAssignments.push(arr)
     }
+    console.log(matchAssignments)
 
-    output.innerText += "\n\n"
-
-    for (let x in missedMatchesTest) {
-        if (missedMatchesTest[x] !== positions.length)
-            output.innerText += "Match " + x + " is being scouted " + missedMatchesTest[x] + " times.\n"
-    }
-
-    
 
 }
